5fe366ffa48ef12ee5bff928befeb0b7fc467512
vars007 <shanbhagvarun55@gmail.com>
2019-11-16 16:48:00 -0600

    Commited word

--- EWA_Amazon/ewa/ewa/spiders/amazon_spider.py
+++ EWA_Amazon/ewa/ewa/spiders/amazon_spider.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from ..items import EwaItem
+from scrapy.http.request import Request
 
 class AmazonSpiderSpider(scrapy.Spider):
     name = 'amazon_s'
     allowed_domains = ['amazon.com']
-    start_urls = ['http://www.amazon.com/dp/030758836X', 'http://www.amazon.com/dp/0439023483', 'http://www.amazon.com/dp/0375831002', 'http://www.amazon.com/dp/038536315X', 'http://www.amazon.com/dp/0439023513', 'http://www.amazon.com/dp/0316055433', 'http://www.amazon.com/dp/0385537859', 'http://www.amazon.com/dp/0007444117', 'http://www.amazon.com/dp/147674355X', 'http://www.amazon.com/dp/0399159347']
+    start_urls = ['http://www.amazon.com/dp/B001HBHNHE']
+    other_urls = ['http://www.amazon.com/dp/B001T7QJ9O', 'http://www.amazon.com/dp/B000S5ODN2', 'http://www.amazon.com/dp/B0010O748Q', 'http://www.amazon.com/dp/B0000C50K3', 'http://www.amazon.com/dp/B002ZYRV2E', 'http://www.amazon.com/dp/B002OKWHVO', 'http://www.amazon.com/dp/B000GCRWCG', 'http://www.amazon.com/dp/B001HBHNHY', 'http://www.amazon.com/dp/B0035L35A8']
 
     def parse(self, response):
         items = EwaItem()
         title = response.xpath('//span[@id="productTitle"]/text()').extract()
         #title = response.xpath('//h1[@id="title"]/span/text()').extract()
         items['product_name'] = ''.join(title).strip()
-
-
         yield items
+        for url in AmazonSpiderSpider.other_urls:
+            yield response.follow(url,callback=self.parse)
